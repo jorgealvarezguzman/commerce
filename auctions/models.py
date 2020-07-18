a58e996b4b6170611aeb84d5bcdcb5250b3fd4f6
Jorge Alfredo Alvarez Guzman <jorgealfredoalvarezguzman@Jorges-MacBook-Pro.local>
2020-07-18 18:45:20 -0500

    Add Models requirement

---
+++ auctions/models.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    pass
+
+
+class Listing(models.Model):
+    title = models.CharField(max_length=64)
+    starting_bid = models.FloatField()
+    description = models.CharField(max_length=256)
+    date_created = models.DateField(auto_now_add=True)
+    image = models.CharField(max_length=2550, blank=True)
+    category = models.CharField(max_length=64, blank=True)
+    listed_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Bid(models.Model):
+    listings = models.ManyToManyField(Listing, related_name="bids")
+    bid = models.FloatField()
+
+
+class Comment(models.Model):
+    listings = models.ManyToManyField(Listing, related_name="comments")
+    comment = models.CharField(max_length=512)
